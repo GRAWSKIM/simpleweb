0d65f28da7399b7b4370974bab0760f13f714c90
JYK <JYK@210.113.14.195>
2020-11-05 22:15:58 +0900

    20201105 work

--- SimpleWebpage/src/main/java/com/simple/contoller/SignInUpController.java
+++ SimpleWebpage/src/main/java/com/simple/contoller/SignInUpController.java
@@ -2,12 +2,23 @@ package com.simple.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SignInUpController {
 
 	@RequestMapping("/")
-	public String Login() {
+	public String gotologin() {
 		return "/signin";
 	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String Login(
+			@RequestParam("id") String id,
+			@RequestParam("password") String password) {
+				
+		return "/home";
+	}
+	
 }
